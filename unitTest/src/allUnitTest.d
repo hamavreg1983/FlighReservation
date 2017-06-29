c9fc847292bb55640f6d6f7eba6649af5da570fb
uv <uv@uv-ThinkCentre-M73>
2017-06-29 09:52:29 +0300

    cleanup

--- unitTest/src/allUnitTest.d
+++
@@ -1,8 +0,0 @@
-src/allUnitTest.o: ../src/allUnitTest.cpp ../../googleTest/gtest/gtest.h \
- ../src/Seat_t_test.cpp ../src/Seat_t.h
-
-../../googleTest/gtest/gtest.h:
-
-../src/Seat_t_test.cpp:
-
-../src/Seat_t.h:
